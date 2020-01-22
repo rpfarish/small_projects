ba3aa21473900db15bfa1f3db9ef869b9fe34833
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-01-21 23:09:41 -0500

    Add files via upload

--- Pong Main2.py
+++ Pong Main2.py
@@ -6,7 +6,7 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#Paddle a
+# Paddle a
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -16,7 +16,7 @@ paddle_a.penup()
 paddle_a.goto(-350,0)
 
 
-# and b
+# Paddle b
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -26,34 +26,43 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
-
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = 1
+ball.dx = .5
+ball.dy = -.5
 
+# Pen
 
-# Function
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  PlayerB: 0", align="center", font="Courier")
 
-
+# Paddle Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
+
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y = y - 20
     paddle_a.sety(y)
 
+
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
+
 
 def paddle_b_down():
     y = paddle_b.ycor()
@@ -63,12 +72,11 @@ def paddle_b_down():
 
 # Keyboard binding
 wn.listen()
-
-
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
+
 # Main game loop
 
 while True:
@@ -79,21 +87,27 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Boarder checking
+    # Top and bottom
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-
-    if ball.ycor() < -290:
+ 
+    elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
 
+    # Left and right
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
-
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
     # Paddle and ball collisions
-    if ball.xcor() < -340 and paddle_a.xcor() + 50 > ball.xcor() > paddle_a.xcor() - 50:
+    if 340 < ball.xcor() < 350 and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+    if (-340 > ball.xcor() > -350) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
         ball.dx *= -1
