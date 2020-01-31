52bfb213af3545a01f0d02fcb9f7b13527f8d35a
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-01-30 20:13:13 -0500

    Add files via upload

--- Main_mo10bo3.py
+++ Main_mo10bo3.py
@@ -1,15 +1,20 @@
 from statistics import mean
-solve_count = 0
+disp_solve_count = 0
+solves = 1
 solve_list = []
-while solve_count < 6:
-    solve_count += 1
-    last_solve = float(input(f"Solve number {str(solve_count)} Time: "))
-    confirm = input("Are You sure?").lower()
-    if confirm == "no":
-        last_solve = float(input("Solve Time: "))
-    solve_list.append(last_solve)
-    print("")
-    if solve_count // 3:
+while disp_solve_count < 6:
+    str_last_solve = input(f"Solve number {str(solves)} Time: ")
+    if str_last_solve != "dnf":
+        last_solve = float(str_last_solve)
+        confirm = input("Are You sure?").lower()
+        solves += 1
+        disp_solve_count += 1
+        if confirm == "no":
+            last_solve = float(input("Solve Time: "))
+        solve_list.append(last_solve)
+        print("")
+    else: solves -= 1
+    if disp_solve_count // 3:
         print(solve_list)
         print("Last mean of 3: " + str(mean(solve_list[-3:])) + "\n")
 
@@ -19,7 +24,7 @@ save.close()
 '''
 
 def print_ave():
-    print(f'Average: {mean(solve_list)} for {solve_count} solves yay')
+    print(f'Average: {mean(solve_list)} for {solves} solves yay')
 print_ave()
 
 
