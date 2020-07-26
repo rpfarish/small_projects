3464dcf2eb13a2a2219e6b92b603b2dad15c5f16
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-07-25 21:25:02 -0400

    update of scratch 14

---
+++ super.py
@@ -0,0 +1,2 @@
+while input("Guess the number between 0 and 30:\n") != "27":pass
+input(input("Enter your name:\n").title() + " is Super" + input("Enter your gender(Man or Woman):\n").lower() + "!")
